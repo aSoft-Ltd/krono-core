af7f6aa0632fe8fa0f82b5e9e3f6551bc94a32ec
andylamax <andylamax@programmer.net>
2023-11-26 12:05:12 +0300

    sunday afternoon sync

--- api/src/jvmMain/kotlin/krono/LocalDateTimeJvmFactory.kt
+++ api/src/jvmMain/kotlin/krono/LocalDateTimeJvmFactory.kt
@@ -7,3 +7,7 @@ import java.time.LocalDateTime
 import java.time.ZoneId
 
 inline fun Clock.currentJavaLocalDateTime() = LocalDateTime.ofInstant(Instant.ofEpochMilli(currentMillisAsLong()), ZoneId.of("UTC"))
+
+inline fun LocalDate.toLocalDate() = java.time.LocalDate.of(year, monthNumber, dayOfMonth)
+
+inline fun java.time.LocalDate.toLocalDate() = LocalDate(year, month.value, dayOfMonth)
